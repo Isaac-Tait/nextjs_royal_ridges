0fe09c85b0c3bb26f95f8c1d6ae3e9b596818d92
Isaac Tait <isaac@mountaintopcoding.com>
2023-11-06 11:36:31 -0500

    chore: update 404 page 😩

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import Link from 'next/link';
 
-import Header from '../(components)/header';
-import Footer from '../(components)/footer';
+import Header from './(components)/Header';
+import Footer from './(components)/Footer';
 
 const NotFoundPage = () => {
   return (
