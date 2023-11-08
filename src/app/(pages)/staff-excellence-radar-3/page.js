9ba955d845e81d62a36275c750eb0fab9687d0d0
Isaac Tait <isaac@mountaintopcoding.com>
2023-11-08 11:10:25 -0500

    feat: add staff excellence pages 🌟

---
+++ src/app/(pages)/staff-excellence-radar-3/page.js
@@ -0,0 +1,147 @@
+'use client';
+import Link from 'next/link';
+
+import Header from '../../(components)/Header';
+import Footer from '../../(components)/Footer';
+
+const SummerStaffTwo = () => {
+  return (
+    <div className='bg-yellow-200'>
+      <Header />
+      <div className='max-w-6xl mx-2 md:mx-auto text-xs md:text-base h-screen overflow-y-scroll'>
+        <h3 className='text-2xl my-6 font-semibold text-center'>
+          Training
+        </h3>
+        <p>
+          Review the staff onsite training dates and summer activities
+          and mark your calendars for the training dates that apply to
+          you. Onsite training is mandatory!
+        </p>
+        <div className='text-center'>
+          <p className='text-lg font-medium mt-2'>
+            Summer Staff Onsite Training Dates
+          </p>
+          <p className='text-lg'>2022</p>
+        </div>
+        <p className='mt-4 text-red-600 font-medium text-xl'>
+          Wrangler Training
+        </p>
+        <p>Who: All senior staff Wranglers are required to attend.</p>
+        <p className='font-bold underline text-lg'>
+          When: Tuesdays May 3rd, 10th, 17th, and 24th, 10 am to 2 pm.
+        </p>
+        <p>
+          What to Bring: Binder, note paper, pen/pencil, CHA manuals
+          if you have them, packed lunch, snack, water bottle. Clothes
+          appropriate for riding and riding boots.
+        </p>
+        <p>
+          Where: Meet in the Main Barn. Training to include safety for
+          riding instruction and skill expansion for teaching.
+        </p>
+        <p className='mt-4 text-red-600 font-medium text-xl'>
+          Supervisor Leadership Training
+        </p>
+        <p>
+          Who: Supervisors, Facilitators and Wranglers are required to
+          attend.
+        </p>
+        <p className='font-bold underline text-lg'>
+          When: Friday June 3rd, 1 pm to 4 pm
+        </p>
+        <p>
+          What to Bring: Snack, water bottle. Be prepared to
+          participate in any camp activity.
+        </p>
+        <p>
+          Where: Meet in the Main Office. Training to include
+          leadership responsibilities and curriculum overview.
+        </p>
+        <p className='mt-4 text-red-600 font-medium text-xl'>
+          Senior Staff Activity Training
+        </p>
+        <p>
+          Who: Supervisors, Facilitators, Wranglers and Senior Staff
+          Group Leaders for all camps are required to attend.
+        </p>
+        <p className='font-bold underline text-lg'>
+          When: Saturday June 4th, 9 am to 4 pm
+        </p>
+        <p>
+          What to Bring: Sack lunch, snack, water bottle. Be prepared
+          to participate in any camp activity with long pants, closed
+          toed shoes.
+        </p>
+        <p>Where: Meet at the lower camp amphitheater.</p>
+        <p className='mt-4 text-red-600 font-medium text-xl'>
+          Senior Staff 1st Aid/CPR and Activity Training
+        </p>
+        <p>
+          Who: Supervisors, Facilitators, Wranglers and Senior Staff
+          Group Leaders for all camps are required to attend.
+        </p>
+        <p className='font-bold underline text-lg'>
+          When: Saturday June 11th, 9 am to 4 pm
+        </p>
+        <p>
+          What to Bring: Sack lunch, snack, water bottle. Be prepared
+          to participate in any camp activity with long pants, closed
+          toed shoes.
+        </p>
+        <p>
+          Where: Meet at the lower camp amphitheater. First Aid/CPR
+          located at the Day Camp Barn 9:30-noon.
+        </p>
+        <p className='mt-4 text-red-600 font-medium text-xl'>
+          All Staff Training
+        </p>
+        <p>
+          Who: Supervisors, Facilitators, Wranglers, Senior Staff
+          Group Leaders and Juniors for all camps are required to
+          attend.
+        </p>
+        <p className='font-bold underline text-lg'>
+          When: Saturday June 18th, 9 am to 4 pm
+        </p>
+        <p>
+          What to Bring: Sack lunch, snack, water bottle. Be prepared
+          to participate in any camp activity with long pants, closed
+          toed shoes.
+        </p>
+        <p>Where: Meet at the lower camp amphitheater.</p>
+        <div className='text-center flex flex-col'>
+          <Link
+            href='/staff-excellence-radar-4'
+            className='text-medium lg:text-2xl my-6 font-semibold w-full lg:w-1/4 mx-auto text-gray-600 hover:underline hover:text-indigo-600'
+          >
+            {' '}
+            {'>'}
+            {'>'} Online training {'>'}
+            {'>'}
+          </Link>
+          <Link
+            href='/staff-excellence-radar-2'
+            className='text-medium lg:text-2xl my-6 font-semibold w-full lg:w-1/4 mx-auto text-gray-600 hover:underline hover:text-indigo-600'
+          >
+            {' '}
+            {'<'}
+            {'<'} Paperwork {'<'}
+            {'<'}
+          </Link>
+          <Link
+            href='/staff-excellence-radar'
+            className='text-medium lg:text-2xl my-6 font-semibold w-full lg:w-1/4 mx-auto text-gray-600 hover:underline hover:text-indigo-600'
+          >
+            {' '}
+            {'<'}
+            {'<'} Beginning {'<'}
+            {'<'}
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default SummerStaffTwo;
---
+++ src/app/(pages)/staff-excellence-radar-4/page.js
@@ -0,0 +1,199 @@
+'use client';
+import Link from 'next/link';
+
+import Header from '../../(components)/Header';
+import Footer from '../../(components)/Footer';
+
+const SummerStaffTwo = () => {
+  return (
+    <div className='bg-yellow-200'>
+      <Header />
+      <div className='max-w-7xl mx-2 md:mx-auto text-xs md:text-base h-screen overflow-y-scroll'>
+        <h3 className='text-2xl my-6 font-semibold text-center'>
+          Online Training
+        </h3>
+        <div className='grid grid-cols-2 gap-12'>
+          {/*Column 1*/}
+          <div className='flex flex-col text-center'>
+            <Link
+              href={'/ser-traditional-adventure-paintball'}
+              className='mx-auto my-4 font-cursive hover:underline text-green-200 hover:text-indigo-500'
+            >
+              <p>
+                TRADITIONAL DAY CAMP STAFF, PAINTBALL STAFF and
+                ADVENTURE CAMP STAFF
+              </p>
+            </Link>
+            <div class='py-4'>
+              <div class='w-full border-t border-gray-800'></div>
+            </div>
+            <Link
+              href={'/ser-high-ropes'}
+              className='mx-auto my-4 font-cursive hover:underline text-green-200 hover:text-indigo-500'
+            >
+              <p>HIGH ROPE STAFF</p>
+            </Link>
+            <div class='py-4'>
+              <div class='w-full border-t border-gray-800'></div>
+            </div>
+            <Link
+              href={'/ser-horsecamp'}
+              className='mx-auto my-4 font-cursive hover:underline text-green-200 hover:text-indigo-500'
+            >
+              <p>HORSE CAMP STAFF</p>
+            </Link>
+            <div class='py-4'>
+              <div class='w-full border-t border-gray-800'></div>
+            </div>
+            <Link
+              href={'/ser-paintball'}
+              className='mx-auto my-4 font-cursive hover:underline text-green-200 hover:text-indigo-500'
+            >
+              <p>PAINTBALL STAFF</p>
+            </Link>
+          </div>
+          {/*Column 2*/}
+          <div className='flex flex-col mr-4 overflow-y-scroll'>
+            <p className='text-center font-semibold text-2xl'>
+              ALL STAFF
+            </p>
+            <p>
+              Everyone (seniors and juniors) must complete the
+              following videos and review the Staff Manual below as
+              well as complete the quiz.
+            </p>
+            {/* Camp Training Intro */}
+            <a
+              href='https://www.youtube.com/watch?v=GQJPGipLLMg'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-green-200 hover:underline hover:text-indigo-400 ml-2 my-2 w-1/2'
+            >
+              Camp Training Introduction
+            </a>
+            {/* Policy and General Rules */}
+            <a
+              href='https://www.youtube.com/watch?v=CuTmtYRsy4U'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-green-200 hover:underline hover:text-indigo-400 ml-2 my-2'
+            >
+              Policy and General Rules
+            </a>
+            {/* Emergency Protocol */}
+            <a
+              href='https://www.youtube.com/watch?v=ySUB4qoOvjM'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-green-200 hover:underline hover:text-indigo-400 ml-2 my-2 w-1/2'
+            >
+              Emergency Protocols
+            </a>
+            {/* Nurse Procedures */}
+            <a
+              href='https://www.youtube.com/watch?v=CAXUWDI99MM'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-green-200 hover:underline hover:text-indigo-400 ml-2 my-2 w-1/2'
+            >
+              Nurse Procedures
+            </a>
+            {/* Cleaning */}
+            <a
+              href='https://www.youtube.com/watch?v=Ww_bnhp5Ask'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-green-200 hover:underline hover:text-indigo-400 ml-2 my-2 w-1/2'
+            >
+              Cleaning
+            </a>
+            {/* Staff Manual */}
+            <a
+              href='https://docs.google.com/document/d/1Lg6t1oQ0ajEVWgxW4fX8xBTxHsr0KkJLVktfB1o0bII/edit?usp=sharing'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-green-200 hover:underline hover:text-indigo-400 ml-2 my-2 w-1/2'
+            >
+              Staff Manual
+            </a>
+            {/* Camp Song Book */}
+            <a
+              href='https://docs.google.com/document/d/1oXGjHUTLZHPHw0UYkk9wVXtq8JBmBGqJKOoEgdV9WgY/edit?usp=sharing'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-green-200 hover:underline hover:text-indigo-400 ml-2 my-2 w-1/2'
+            >
+              Song Book
+            </a>
+            {/* All Staff Quiz */}
+            <a
+              href='https://docs.google.com/forms/d/e/1FAIpQLSf3l0Bsv67uskSbCEdXbhilple-K2Duopa_afeH0XIxi01oyQ/viewform?usp=sf_link'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-green-200 hover:underline hover:text-indigo-400 ml-2 my-2 w-1/2'
+            >
+              ALL STAFF QUIZ
+            </a>
+            <p>
+              Please also read over and study this year&#39;s camper
+              Bible Curriculum from the link below. It is important
+              that you are familiar enough with the curriculum to be
+              able to teach it to the campers and not just read the
+              paper.
+            </p>
+            {/* Bible Curriculum */}
+            <a
+              href='https://docs.google.com/document/d/12CwxRjw0YVrUDOFHACI1fE1TlanHeucJ-XwQFeEX0ac/edit?usp=sharing'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-green-200 hover:underline hover:text-indigo-400 ml-2 my-2 w-1/2'
+            >
+              Bible Curriculum 2022
+            </a>
+            {/* Extended Bible Curriculum */}
+            <a
+              href='https://docs.google.com/document/d/1sXUM8Vr375OF_mLfceHkDDf6Ph1P1R-qVbaBt3rvDeE/edit?usp=sharing'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='text-green-200 hover:underline hover:text-indigo-400 ml-2 my-2 w-1/2'
+            >
+              Day Horse Extended Stay Curriculum 2022
+            </a>
+          </div>
+        </div>
+        <div className='text-center flex flex-col'>
+          <Link
+            href='/staff-excellence-radar-3'
+            className='text-medium lg:text-2xl my-6 font-semibold w-full lg:w-1/4 mx-auto text-gray-600 hover:underline hover:text-indigo-600'
+          >
+            {' '}
+            {'<'}
+            {'<'} Training {'<'}
+            {'<'}
+          </Link>
+          <Link
+            href='/staff-excellence-radar-2'
+            className='text-medium lg:text-2xl my-6 font-semibold w-full lg:w-1/4 mx-auto text-gray-600 hover:underline hover:text-indigo-600'
+          >
+            {' '}
+            {'<'}
+            {'<'} Paperwork {'<'}
+            {'<'}
+          </Link>
+          <Link
+            href='/staff-excellence-radar'
+            className='text-medium lg:text-2xl my-6 font-semibold w-full lg:w-1/4 mx-auto text-gray-600 hover:underline hover:text-indigo-600'
+          >
+            {' '}
+            {'<'}
+            {'<'} Beginning {'<'}
+            {'<'}
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default SummerStaffTwo;
